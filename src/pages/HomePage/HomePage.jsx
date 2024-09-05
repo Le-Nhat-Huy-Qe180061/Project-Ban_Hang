060e72b30f7cf0c6618d7bfb257c915992d2b63d
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-09-05 11:09:55 +0700

    [#2 finish Deive_component]

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,22 @@
 
+import TypeProduct from "../../components/TypeProduct/TypeProduct";
+import { WarpperTypeProduct } from "./style";
+
 
 const HomePage = () => {
-    return(
+    const arr = ['TV', 'Tu Lanh', 'Lap top']
+    return (
         <>
-            HomePage
+            <div style={{ padding: '0 120px' }}>
+                <WarpperTypeProduct>
+                    {arr.map((item, _) => {
+                        return (
+                            <TypeProduct name={item} key={item} />
+                        )
+                    })}
+                </WarpperTypeProduct>
+                HomePage
+            </div>
         </>
     )
 }
