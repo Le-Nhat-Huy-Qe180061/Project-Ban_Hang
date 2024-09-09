79e02f40875ffd97f3b4a92a911cc2da94972ab8
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-09-09 22:14:33 +0700

    Done interview

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,11 @@
 import NotfoundPage from '../components/NotfoundPage/NotfoundPage.jsx';
 import HomePage from '../pages/HomePage/HomePage.jsx';
 import OrderPage from '../pages/OrderPage/OrderPage.jsx';
+import ProductDetailsPage from '../pages/ProductDetailsPage/ProductDetailsPage.jsx';
 import ProductPage from '../pages/ProductPage/ProductPage.jsx';
+import SignInPage from '../pages/SignInPage/SignInPage.jsx';
+import SignUpPage from '../pages/SignUpPage/SignUpPage.jsx';
+import TypeProductPage from '../pages/TypeProductPage/TypeProductPage.jsx';
 
 
 export const routes = [
@@ -19,6 +23,26 @@ export const routes = [
     {
         path: "/products",
         page: ProductPage,
+        isShowHeader: true,
+    },
+    {
+        path: "/:type",
+        page: TypeProductPage,
+        isShowHeader: true,
+    },
+    {
+        path: "/sign-in",
+        page: SignInPage,
+        isShowHeader: true,
+    },
+    {
+        path: "/sign-up",
+        page: SignUpPage,
+        isShowHeader: true,
+    },
+    {
+        path: "/product-detail",
+        page: ProductDetailsPage,
         isShowHeader: true,
     },
     {
