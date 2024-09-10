4ac97c24f27ca329cd3c5f6ab3da8130615b0de3
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-09-10 07:06:05 +0700

    Done Page detail Product

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ProductDetailComponent from '../../components/ProductDetailComponent/ProductDetailComponent'
 
 const ProductDetailsPage = () => {
   return (
-    <div>ProductDetailsPage</div>
+    
+        <div style={{padding: '0 120px', background:'#efefef', height: '1000px'}}>
+            <h5>Trang chủ</h5>
+            <ProductDetailComponent />
+        </div>
+
   )
 }
 
